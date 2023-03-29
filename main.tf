119660ed94930738779a6b205d4f1ca1392286cd
mkaesz <mskaesz@msk.pub>
2023-03-29 15:18:05 +0200

    added terraform code

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ provider "azurerm" {
   features {}
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = "Vault-Azure"
+  location            = "West Europe"
+  address_space       = ["10.0.0.0/16"]
 }
